ac4968efc0729c808a0926d116e956af8a9849f2
Himanshu Shukla <itachiuchiha@Himanshus-MacBook-Air.local>
2023-11-08 00:48:05 +0530

    new : name added in home section

--- src/components/home/index.jsx
+++ src/components/home/index.jsx
@@ -2,7 +2,11 @@ import React from "react";
 import "./index.scss";
 
 const Home = () => {
-  return <div id="home">Home</div>;
+  return (
+    <div id="home">
+      <h1 id="user-detail-name">{`> HIMANSHU SHUKLA`}</h1>
+    </div>
+  );
 };
 
 export default Home;
