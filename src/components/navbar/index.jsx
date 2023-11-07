286458db8217fa0d8eac69c92cb8f8641e8d376f
Himanshu Shukla <itachiuchiha@Himanshus-MacBook-Air.local>
2023-11-07 23:51:23 +0530

    new : Navbar added

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -6,19 +6,19 @@ import "./index.scss";
 const Navbar = () => {
   return (
     <nav id="nav-menu">
-      <Link smooth to="/#home">
+      <Link smooth to="/#home" className="nav-link home">
         Home
       </Link>
-      <Link smooth to="/#about">
+      <Link smooth to="/#about" className="nav-link about">
         About
       </Link>
-      <Link smooth to="/#skills">
+      <Link smooth to="/#skills" className="nav-link skills">
         Skills
       </Link>
-      <Link smooth to="/#contact">
+      <Link smooth to="/#contact" className="nav-link contact">
         Contact
       </Link>
-      <Link smooth to="/#projects">
+      <Link smooth to="/#projects" className="nav-link projects">
         Projects
       </Link>
     </nav>
