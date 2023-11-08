253681accd3e61cad493c075673cff82e7480895
Himanshu Shukla <itachiuchiha@Himanshus-MacBook-Air.local>
2023-11-08 15:09:30 +0530

    new : home section

--- src/components/home/index.jsx
+++ src/components/home/index.jsx
@@ -1,10 +1,28 @@
 import React from "react";
+import { AiFillFolder } from "react-icons/ai";
 import "./index.scss";
 
 const Home = () => {
   return (
     <div id="home">
-      <h1 id="user-detail-name">{`>HIMANSHU SHUKLA`}</h1>
+      <div id="terminal-header">
+        <div id="buttons">
+          <button id="red"></button>
+          <button id="yellow"></button>
+          <button id="green"></button>
+        </div>
+        <span>
+          <AiFillFolder
+            style={{ height: "2em", width: "2em", color: "#48CBF8" }}
+          />
+          <h3>_Himanshu's termial</h3>
+        </span>
+      </div>
+      <div id="user-detail-intro">
+        <h1 className="typing">{`>Hello World!`}</h1>
+        <p id="welcome-msg">{`>Welcome to my Portfolio.`}</p>
+        <p id="intro">{`>I'm a Full Stack Developer!`}</p>
+      </div>
     </div>
   );
 };
